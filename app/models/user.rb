3c828429a6a63c54c8627f501f09acd84a21468f
Ramesh Jha <rameshjha420@gmail.com>
2012-03-07 01:37:14 +0530

    Done some password encryption related work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,40 @@
+require 'digest'
 class User < ActiveRecord::Base
-	attr_accessible :name, :email
+
+	attr_accessor :password
+	attr_accessible :name, :email, :password, :password_confirmation
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 	validates :name, :presence => true, :length => {:maximum => 50}
 	validates :email, :presence => true,
 					  :format => {:with => email_regex},
 					  :uniqueness => {:case_sensitive => false}
-	has_many :microposts
+
+	validates :password, :presence => true,
+						 :confirmation => true,
+						 :length => {:within => 6..40}
+
+	before_save :encrypt_password
+	def has_password(submitted_password)
+		#encrypt the submitted password and compare with stored encrypted password
+		encrypted_password = encrypt(submitted_password)
+	end
+
+	private
+
+		def encrypt_password
+			self.salt = make_salt if new_record?
+			self.encrypted_password = encrypt(password)
+		end
+
+		def encrypt(string)
+			#Create a hash with string
+			secure_hash("#{salt}--#{string}")
+		end
+		def make_salt
+			secure_hash("#{Time.now.utc}--#{password}")
+		end
+		def secure_hash(string)
+			Digest::SHA2.hexidegest(string)
+		end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120306185713) do
+ActiveRecord::Schema.define(:version => 20120306195445) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(:version => 20120306185713) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
