adf6cee924abf150341a2ad51ddfee7f826ef68b
Ramesh Jha <rameshjha420@gmail.com>
2012-03-07 00:50:45 +0530

    Finished out the first cut on the user model

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,23 @@ Mblog::Application.routes.draw do
   get "pages/home"
 
   get "pages/contact"
-  
+
   get "pages/about"
-  
+
   get "pages/help"
 
   resources :microposts
 
-  #resources :users
-  
+  resources :users
+  match '/signup', :to => 'users#new'
   root :to => 'pages#home'
   match '/about', :to => 'pages#about'
   match '/contact', :to => 'pages#contact'
   match '/help', :to => 'pages#help'
   match '/signup', :to => 'users#new'
-  
+
   match '/', :to => 'pages#home'
-  
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
